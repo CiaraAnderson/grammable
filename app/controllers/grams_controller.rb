9104f5f46b947cc5f63fec6819eaff8b20b73327
Ciara Anderson <thelaylinguist@gmail.com>
2019-08-26 03:29:09 +0000

    Gram Detail page

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -10,6 +10,12 @@ class GramsController < ApplicationController
 
   end
 
+  def show
+  @gram = Gram.find_by_id(params[:id])
+  if @gram.blank?
+    render plain: 'Not Found :(', status: :not_found
+  end
+ end
 
   def create
      @gram = current_user.grams.create(gram_params)
@@ -28,5 +34,5 @@ class GramsController < ApplicationController
   end
 
 
-
+  
 end
