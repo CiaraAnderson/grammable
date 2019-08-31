f72684eab30a5999806dc92efdac98c139e2d455
Ciara Anderson <thelaylinguist@gmail.com>
2019-08-31 18:55:20 +0000

    Carrierwave updates

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,7 @@
 class Gram < ApplicationRecord
   validates :message, presence: true
-
+  
+  mount_uploader :picture, PictureUploader
   belongs_to :user
-   mount_uploader :picture, PictureUploader
+
 end
