a18378b9262c2dc8f6a5c0bfc122d4cc7bf99d55
Ciara Anderson <thelaylinguist@gmail.com>
2019-08-31 02:20:30 +0000

    Carrierwave

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,12 +2,13 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-
+    @grams = Gram.all
   end
 
 
   def new
     @gram = Gram.new
+
   end
 
  def create
@@ -57,7 +58,7 @@ class GramsController < ApplicationController
   private
      
   def gram_params
-      params.require(:gram).permit(:message)
+      params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :grams
+  mount_uploader :picture, PictureUploader
 end
